e488805778f786fd2c18d5553549a767ae0a236e
lsqlll <lsq355720851@163.com>
2025-12-09 17:23:07 +0800

    移除大量非必要内容(build,cmake等等)

--- test/build/CMakeFiles/isEmptytest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for isEmptytest.
-# This may be replaced when dependencies are built.
--- test/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/dailyReport/api/DB_cpp/test
-# Build directory: /data/dailyReport/api/DB_cpp/test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(isEmptytest "/data/dailyReport/api/DB_cpp/test/build/isEmptytest")
-set_tests_properties(isEmptytest PROPERTIES  _BACKTRACE_TRIPLES "/data/dailyReport/api/DB_cpp/test/CMakeLists.txt;35;add_test;/data/dailyReport/api/DB_cpp/test/CMakeLists.txt;0;")
