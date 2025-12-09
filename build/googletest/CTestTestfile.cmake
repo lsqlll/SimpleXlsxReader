665411f2840275ab3d33dabf6932e3bb61fc3f8e
lsqlll <lsq355720851@163.com>
2025-12-09 20:01:36 +0800

    删除大量无用文件

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Cpp/simpleTableReader/third-party/googletest
-# Build directory: E:/Cpp/simpleTableReader/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Cpp/simpleTableReader/third-party/googletest/googletest
-# Build directory: E:/Cpp/simpleTableReader/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
